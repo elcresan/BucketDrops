fb104e7b9d543ea1a43086f56bfadedf7ab0d509
Eliu <elcresan@gmail.com>
2016-02-06 14:22:56 +0100

    Dialog fragment to add drops added. Style and functionality are still pending to be added.

--- app/src/main/java/com/supersmiley/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/supersmiley/bucketdrops/ActivityMain.java
@@ -3,13 +3,27 @@ package com.supersmiley.bucketdrops;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity{
 
     Toolbar mToolbar;
+    Button mBtnAdd;
+    private View.OnClickListener mBtonAddListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            showDialogAdd();
+        }
+    };
+
+    private void showDialogAdd() {
+        DialogAdd dialog = new DialogAdd();
+        dialog.show(getSupportFragmentManager(), "Add");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,9 @@ public class ActivityMain extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.btn_add);        
+        mBtnAdd.setOnClickListener(mBtonAddListener);
+
         setSupportActionBar(mToolbar);
         initBackgroundImage();
     }
