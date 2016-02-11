dd87ef9833b40670d11d06867483389248556d70
Eliu <elcresan@gmail.com>
2016-02-11 21:09:28 +0100

    Menu item added without functionality.

--- app/src/main/java/com/supersmiley/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/supersmiley/bucketdrops/ActivityMain.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -119,6 +121,33 @@ public class ActivityMain extends AppCompatActivity{
     protected void onStop() {
         super.onStop();
         mResults.removeChangeListener(mChangeListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_add:
+                break;
+            case R.id.action_sort_descending_date:
+                break;
+            case R.id.action_sort_ascending_date:
+                break;
+            case R.id.action_show_complete:
+                break;
+            case R.id.action_show_incomplete:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void initBackgroundImage(){
