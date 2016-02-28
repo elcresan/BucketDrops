ef24f1dacddf0380efc231d3c2ec08fe5c9e66d1
Eliu <elcresan@gmail.com>
2016-02-28 23:25:40 +0100

    Custom date picker widget implemented.

--- app/src/main/java/com/supersmiley/bucketdrops/widgets/BucketPickerView.java
+++ app/src/main/java/com/supersmiley/bucketdrops/widgets/BucketPickerView.java
@@ -1,18 +1,25 @@
 package com.supersmiley.bucketdrops.widgets;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.supersmiley.bucketdrops.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class BucketPickerView extends LinearLayout implements View.OnTouchListener {
+    public static final int LEFT = 0;
+    public static final int TOP = 1;
+    public static final int  RIGHT = 2;
+    public static final int  BOTTOM = 3;
     private Calendar mCalendar;
     private TextView mTextDate;
     private TextView mTextMonth;
@@ -87,18 +94,120 @@ public class BucketPickerView extends LinearLayout implements View.OnTouchListen
      */
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+        switch(v.getId()){
+            case R.id.tv_date:
+                processEventsFor(mTextDate , event);
 
-        switch (event.getAction()){
-            case MotionEvent.ACTION_DOWN:
                 break;
-            case MotionEvent.ACTION_MOVE:
+            case R.id.tv_month:
+                processEventsFor(mTextMonth , event);
+
                 break;
-            case MotionEvent.ACTION_UP:
-                break;
-            case MotionEvent.ACTION_CANCEL:
+            case R.id.tv_year:
+                processEventsFor(mTextYear , event);
+
                 break;
         }
 
         return true;
+    }
+
+    private void processEventsFor(TextView textView, MotionEvent event){
+        Drawable[] drawables = textView.getCompoundDrawables();
+
+        if(hasDrawableTop(drawables) && hasDrawableBottom(drawables)){
+            Rect topBounds = drawables[TOP].getBounds();
+            Rect bottomBounds = drawables[BOTTOM].getBounds();
+            float x = event.getX();
+            float y = event.getY();
+
+            if(topDrawableHit(textView, topBounds.height(), x, y)){
+                if(isActionDown(event)){
+                    increment(textView.getId());
+                }
+            } else if( bottomDrawableHit(textView, bottomBounds.height(), x, y)){
+                if(isActionDown(event)){
+                    decrement(textView.getId());
+                }
+            }
+        }
+    }
+
+    private void increment(int id){
+        switch(id){
+            case R.id.tv_date:
+                mCalendar.add(Calendar.DATE, 1);
+
+                break;
+            case R.id.tv_month:
+                mCalendar.add(Calendar.MONTH, 1);
+
+                break;
+            case R.id.tv_year:
+                mCalendar.add(Calendar.YEAR, 1);
+
+                break;
+        }
+
+        setTexts(mCalendar);
+    }
+
+    private void decrement(int id){
+        switch(id){
+            case R.id.tv_date:
+                mCalendar.add(Calendar.DATE, -1);
+
+                break;
+            case R.id.tv_month:
+                mCalendar.add(Calendar.MONTH, -1);
+
+                break;
+            case R.id.tv_year:
+                mCalendar.add(Calendar.YEAR, -1);
+
+                break;
+        }
+
+        setTexts(mCalendar);
+    }
+
+
+    private void setTexts(Calendar calendar) {
+        int date = calendar.get(Calendar.DATE);
+        int year = calendar.get(Calendar.YEAR);
+
+        mTextDate.setText(date + "");
+        mTextMonth.setText(mFormatter.format(mCalendar.getTime()));
+        mTextYear.setText(year + "");
+    }
+
+    private boolean isActionDown(MotionEvent event){
+        return event.getAction() == MotionEvent.ACTION_DOWN;
+    }
+
+    private boolean topDrawableHit(TextView textView, int drawableHeight, float x, float y){
+        int xmin = textView.getPaddingLeft();
+        int xmax = textView.getWidth() - textView.getPaddingRight();
+        int ymin = textView.getPaddingTop();
+        int ymax = textView.getPaddingTop() + drawableHeight;
+
+        return x > xmin && x < xmax && y > ymin && y < ymax;
+    }
+
+    private boolean bottomDrawableHit(TextView textView, int drawableHeight, float x, float y){
+        int xmin = textView.getPaddingLeft();
+        int xmax = textView.getWidth() - textView.getPaddingRight();
+        int ymax = textView.getHeight() - textView.getPaddingBottom();
+        int ymin = ymax - drawableHeight;
+
+        return x > xmin && x < xmax && y > ymin && y < ymax;
+    }
+
+    private boolean hasDrawableTop(Drawable[] drawables){
+        return drawables[TOP] != null;
+    }
+
+    private boolean hasDrawableBottom(Drawable[] drawables){
+        return drawables[BOTTOM] != null;
     }
 }
